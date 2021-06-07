192f708a8800947f01867b8dfd786cbb04d6bec1
Ivan Dzherikhov <jodsonjod@gmail.com>
2021-06-07 12:34:30 +0300

    Добавьте файлы проекта.

---
+++ Figure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sem2lab5_
+{
+    public abstract class Figure
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public abstract void DrawBlack(Panel f);
+        public abstract void HideDrawingBackGround(Panel f);
+
+        private Random random;
+        public void MoveRight(Panel f)
+        {
+            random = new Random();
+            int xRight = random.Next(10, 50);
+            HideDrawingBackGround(f);
+            x += xRight;
+            DrawBlack(f);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace sem2lab5_
+{
+    static class Program
+    {   
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
